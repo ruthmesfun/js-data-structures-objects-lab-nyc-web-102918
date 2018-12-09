320d3e1bad82d1c72fab089f3e476d1f5ce37c1f
ruthmesfun <ruth.mesfun@gmail.com>
2018-12-09 08:16:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+let driver = {};
+
+let updateDriverWithKeyAndValue = (driver, key, value) =>{
+    
+    let newDriver = {...driver};
+        
+    newDriver[key] = value;
+
+    return newDriver;
+}
+
+let destructivelyUpdateDriverWithKeyAndValue = (driver, key, value) =>{
+
+    driver[key] = value;
+
+    return driver;
+}
+
+let deleteFromDriverByKey = (driver, key) => {
+    
+    let newDriver = {...driver};
+
+    delete newDriver[key];
+
+    return newDriver;
+}
+
+let destructivelyDeleteFromDriverByKey = (driver, key) => {
+    delete driver[key];
+
+    return driver;
+}
